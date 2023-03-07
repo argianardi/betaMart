0788d1cb8e512759128aae6ea56928eec1b8f8b0
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-07 14:12:47 +0700

    add app.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./utils/redux/store/store";
+import App from "./App";
 import "./styles/index.css";
 
 const container = document.getElementById("root");
