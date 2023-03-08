c056ea3c7b80401a44ceb80915337fd4782efa73
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-08 23:08:45 +0700

    add slider reducer

--- src/utils/redux/store/store.js
+++ src/utils/redux/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import sliderReducer from "../features/sliderSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { slider: sliderReducer },
 });
