99b4d8e1d42d802576325aee2b01f2aab5578cc7
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-09 01:24:30 +0700

    add dot slide reducer

--- src/utils/redux/features/sliderSlice.js
+++ src/utils/redux/features/sliderSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { sliderData } from "../../../assets/data/dummyData";
+
 // initial state
 const initialState = {
   value: 1,
@@ -12,14 +13,15 @@ export const sliderSlice = createSlice({
   initialState,
   reducers: {
     nextSlide: (state, action) => {
-      console.log("action", action);
-      console.log("state", state);
       state.value = action.payload > state.length ? 1 : action.payload;
     },
     prevSlide: (state, action) => {
       state.value = action.payload < 1 ? state.length : action.payload;
     },
-    dotSlide() {},
+    dotSlide: (state, action) => {
+      const slide = action.payload;
+      state.value = slide;
+    },
   },
 });
 
