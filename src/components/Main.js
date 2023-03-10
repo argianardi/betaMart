14c5c3c8fbe5241bc5127db16e19d621f7ef7af5
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-10 10:54:12 +0700

    add navigate buttons component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import NavigateButtons from "./NavigateButtons";
 import Slider from "./Slider";
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
     <div>
       <Header />
       <Slider />
+      <NavigateButtons />
     </div>
   );
 };
