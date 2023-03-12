5ad296d52b612be8aec5427c9be0a1d507afadbf
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-12 23:44:22 +0700

    add product slice

---
+++ src/utils/redux/features/productsSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { storeData } from "../../../assets/data/dummyData";
+
+const initialState = {
+  filteredProducts: storeData,
+  singleProduct: [],
+};
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    // Filter Product Reducer
+    filterProducts: (state, action) => {
+      const filter = storeData.filter(
+        (product) => product.type === action.payload
+      );
+      state.filteredProducts = filter;
+    },
+    // Single product Reducer
+    singleProduct: (state, action) => {
+      const oneProduct = storeData.filter(
+        (product) => product.id === action.payload
+      );
+      state.singleProduct = oneProduct;
+    },
+  },
+});
+
+export const { filterProducts, singleProduct } = productsSlice.actions;
+export default productsSlice.reducer;
