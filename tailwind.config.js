09c50d29808ebcf3b7f7271c1f1a4b93dd5c66ed
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-18 10:20:02 +0700

    custom breack point

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screen: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
+
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
