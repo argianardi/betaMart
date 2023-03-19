c101ddcd1b35db5e43a9ba9bb10d419a208eff15
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-19 09:48:24 +0700

    fix custom break point

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,17 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      xs: "320px",
+      sm: "375px",
+      sml: "500px",
+      mdm: "667px",
+      mdl: "768px",
+      lgm: "960px",
+      lgl: "1024px",
+      xlm: "1280px",
+    },
     extend: {
-      screen: {
-        xs: "320px",
-        sm: "375px",
-        sml: "500px",
-        md: "667px",
-        mdl: "768px",
-        lg: "960px",
-        lgl: "1024px",
-        xl: "1280px",
-      },
-
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
