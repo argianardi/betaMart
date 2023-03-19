f79bf0749d44d74d806d2a02267bb07bd490023f
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-19 13:33:59 +0700

    add interval slide

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,23 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BsChevronDoubleLeft, BsChevronDoubleRight } from "react-icons/bs";
 import { sliderData } from "../assets/data/dummyData";
 
 const Slider = () => {
-  const [curentIndex, setCurentIndex] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState(1);
+
+  // Interval slider
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex(currentIndex < sliderData.length ? currentIndex + 1 : 1);
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, [currentIndex]);
 
   // Next slide handle
   const nextSlide = (index) => {
-    setCurentIndex(index > sliderData.length ? 1 : index);
+    setCurrentIndex(index > sliderData.length ? 1 : index);
   };
 
   // Previous slide handle
   const prevSlide = (index) => {
-    setCurentIndex(index < 1 ? sliderData.length : index);
+    setCurrentIndex(index < 1 ? sliderData.length : index);
   };
 
   // Dot slide handle
   const dotSlide = (index) => {
-    setCurentIndex(index);
+    setCurrentIndex(index);
   };
 
   return (
@@ -27,7 +35,7 @@ const Slider = () => {
         {sliderData.map((item) => (
           <div key={item.id}>
             <div className="absolute w-full h-full transition-all duration-500">
-              {item.id === curentIndex && (
+              {item.id === currentIndex && (
                 <img
                   src={item.img}
                   alt="shoes"
@@ -37,18 +45,18 @@ const Slider = () => {
             </div>
             <div className="absolute mx-auto bg-black/5 top-5 inset-x-8 bg-blur">
               <p className="text-sm font-semibold leading-none tracking-normal text-center text-white sml:text-lg sm:text-base mdl:text-2xl lgl:text-4xl font-inter">
-                {item.id === curentIndex && item.text}
+                {item.id === currentIndex && item.text}
               </p>
             </div>
           </div>
         ))}
         {/* dot slider */}
-        <div className="absolute flex left-2 bottom-2 bg-blue-gray-200">
+        <div className="absolute flex left-2 bottom-2">
           {sliderData.map((dot, index) => (
             <div className="mr-4" key={dot.id}>
               <div
                 className={
-                  index + 1 === curentIndex
+                  index + 1 === currentIndex
                     ? "bg-green-300 rounded-full p-1 cursor-pointer"
                     : "bg-white rounded-full p-1 cursor-pointer"
                 }
@@ -61,13 +69,13 @@ const Slider = () => {
         <div>
           <button
             className="absolute top-[50%] right-2 bg-white rounded-full p-2"
-            onClick={() => nextSlide(curentIndex + 1)}
+            onClick={() => nextSlide(currentIndex + 1)}
           >
             <BsChevronDoubleRight className="w-3 h-3 md:h-4 md:w-4 xl:w-6 xl:h-6" />
           </button>
           <button
             className="absolute top-[50%] left-2 bg-white rounded-full p-2"
-            onClick={() => prevSlide(curentIndex - 1)}
+            onClick={() => prevSlide(currentIndex - 1)}
           >
             <BsChevronDoubleLeft className="w-3 h-3 md:h-4 md:w-4 xl:w-6 xl:h-6" />
           </button>
