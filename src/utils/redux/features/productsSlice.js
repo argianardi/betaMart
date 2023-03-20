f9f58e705e3af15d4a25d1afc570edb2155dcdc6
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-20 14:22:36 +0700

    add product reducer

--- src/utils/redux/features/productsSlice.js
+++ src/utils/redux/features/productsSlice.js
@@ -1,32 +1,52 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { storeData } from "../../../assets/data/dummyData";
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
+} from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = {
-  filteredProducts: [],
-  singleProduct: [],
-  products: storeData,
-};
+// Base URL API
+const apiUrl = "https://dummyjson.com/products";
+
+export const getAllProducts = createAsyncThunk(
+  "products/getAllProducts",
+  async () => {
+    const response = await axios.get(apiUrl);
+    return response.data.products;
+  }
+);
+
+const productsAdapter = createEntityAdapter({
+  selectId: (product) => product.id,
+});
+
+const initialState = productsAdapter.getInitialState({
+  status: "idle",
+  error: null,
+});
 
 const productsSlice = createSlice({
   name: "products",
   initialState,
-  reducers: {
-    // Filter Product Reducer
-    filterProducts: (state, action) => {
-      const filter = storeData.filter(
-        (product) => product.type === action.payload
-      );
-      state.filteredProducts = filter;
-    },
-    // Single product Reducer
-    singleProduct: (state, action) => {
-      const oneProduct = storeData.filter(
-        (product) => product.id === action.payload
-      );
-      state.singleProduct = oneProduct;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    // get all products
+    builder
+      .addCase(getAllProducts.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getAllProducts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        productsAdapter.setAll(state, action.payload);
+      })
+      .addCase(getAllProducts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
   },
 });
 
-export const { filterProducts, singleProduct } = productsSlice.actions;
 export default productsSlice.reducer;
+export const productsSelectors = productsAdapter.getSelectors(
+  (state) => state.products
+);
