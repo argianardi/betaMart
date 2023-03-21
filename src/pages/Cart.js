e970423583eb36c0eb69f4cb67343addca25adeb
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-03-22 00:28:56 +0700

    slicing cart page and add remove from cart action

---
+++ src/pages/Cart.js
@@ -0,0 +1,107 @@
+import React from "react";
+import { Button, Card, Tooltip } from "@material-tailwind/react";
+import { useDispatch, useSelector } from "react-redux";
+import { MdDelete } from "react-icons/md";
+import Header from "../components/Header";
+import {
+  AiFillMinusCircle,
+  AiOutlineMinusCircle,
+  AiOutlinePlusCircle,
+} from "react-icons/ai";
+import { removeFromCart } from "../utils/redux/features/cartSlice";
+
+const Cart = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.cart.products);
+  const grandTotal = useSelector((state) => state.cart.grandTotal);
+  console.log("products in cart", products);
+
+  return (
+    <>
+      <Header />
+      <h2 className="mt-2 ml-2 text-xl font-bold mdl:ml-4 lgm:ml-10 lgl:ml-20 xlm:ml-44">
+        Cart
+      </h2>
+      {products.length > 0 ? (
+        <div className="relative px-2 mdl:px-4 lgm:px-10 lgl:px-20 xlm:px-44">
+          {/* Product details */}
+          <div>
+            {products.map((product) => (
+              <Card
+                key={product.id}
+                className="w-full p-1 my-6 mdl:w-[500px] lgm:w-[600px]"
+              >
+                <div className="justify-between block mdm:flex">
+                  <div className="flex w-full mdm:w-96">
+                    <img
+                      src={product.image}
+                      alt={product.title}
+                      className="object-cover w-20 h-20 rounded-sm"
+                    />
+                    <h4 className="mt-2 ml-2 text-sm leading-none tracking-normal text-gray-900 font-inter">
+                      {product.title}
+                    </h4>
+                  </div>
+
+                  {/* Price and Buttons */}
+                  <div className="flex items-center justify-end mx-2">
+                    <div className="flex mr-10">
+                      <p className="pr-2 mr-2 font-bold border-r-2 border-gray-400">
+                        ${product.price}
+                      </p>
+                      <Tooltip
+                        content="Remove from the Cart"
+                        placement="bottom"
+                      >
+                        <button
+                          onClick={() => dispatch(removeFromCart(product.id))}
+                        >
+                          <MdDelete color="red" className="text-2xl" />
+                        </button>
+                      </Tooltip>
+                    </div>
+                    <div className="flex items-center">
+                      <button>
+                        <AiOutlineMinusCircle className="text-lg" />
+                      </button>
+                      <p className="mx-2">{product.amount}</p>
+                      <button>
+                        <AiOutlinePlusCircle className="text-lg" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            ))}
+          </div>
+
+          {/* Grand total and buy buttons  */}
+          <div className="sticky mdl:abolute bottom-0 flex flex-row items-center justify-between w-full p-2 mdl:flex-col bg-blur bg-white/40 mdl:left-[100%] mdl:bottom-7 mdl:w-52  mdl:border-[1px] border-gray-400">
+            <p className="flex flex-col w-full pt-2 text-base leading-none tracking-normal mdl:justify-between font-inter mdl:flex-row">
+              <span className="text-sm font-bold text-gray-800">
+                Grand Total
+              </span>
+              <span className="mt-1 font-bold text-black mdl:mt-0">
+                ${grandTotal}{" "}
+              </span>
+            </p>
+            <Button color="green" className="w-full mt-0 mdl:mt-2">
+              Buy (100)
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1 className="py-4 text-3xl font-bold leading-none tracking-normal text-black font-inter">
+            Your cart is empty
+          </h1>
+          <p className="text-base leading-none tracking-normal text-black font-inter">
+            Add some products
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Cart;
